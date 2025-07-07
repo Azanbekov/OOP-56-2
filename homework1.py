c89b10923990eef5d892021a56c51a04b92a8449
your email <erzanazanbekov@gmail.com>
2025-07-07 22:27:01 +0600

    HW 1 add.

---
+++ homework1.py
@@ -0,0 +1,17 @@
+class Car:
+    def __init__(self, brand, model, speed, fuel, year):
+        self.brand = brand
+        self.model = model
+        self.speed = speed
+        self.fuel = fuel
+        self.year = year
+    def introduse(self):
+        return print(f"машина {self.brand} {self.model} {self.year} года может разгонятся до {self.speed} ")
+
+Mersedes_benz = Car("Mersedes_benz", "cls63", "350 km/h", "petrol", "2020")
+BMW = Car("BMW", "M5", "330km/h", "diesel", "2019" )
+Mersedes_benz.introduse()
+BMW.introduse()
+print(Mersedes_benz.brand)
+print(f"{BMW.brand} медленне чем {Mersedes_benz.brand}")
+some_text = "Some text"
--- lesson1.py
+++ lesson1.py
@@ -1,12 +1,19 @@
-class Hero:
-#конструктор класса
-    def __init__(self, name, hp,lvl):
-         self.name = name
-         self.lvl = lvl
-         self.hp = hp
-#метод класса
-    def introduse(self):
-         return print(f"привет меня зовут{self.name}")
+# class Hero:
+# #конструктор класса
+#     def __init__(self, name, hp,lvl):
+#          #атрибуты класса
+#          self.name = name
+#          self.lvl = lvl
+#          self.hp = hp
+# #метод класса
+#     def introduse(self):
+#          return print(f"привет меня зовут {self.name}")
+# #обьект|экземпляр класса
+# kirito = Hero("Kirito", "100", "1000") 
 
-kirito = Hero("Kirito", "100", "1000")
-kirito.introduse()
+# kirito.introduse()
+# asuna.introduse()
+# print(kirito.name)
+      
+asuna = Hero("asuna", "10", "100") 
+some_text = "Some text"
